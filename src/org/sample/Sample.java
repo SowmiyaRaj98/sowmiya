10f54574e9a69960498892c50542d6df82306a6f
BALAJI RAJ <sowmyarajendran98@gmail.com>
2026-01-01 17:12:36 +0530

    ProjectJava

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int a=2021;
+		String s="Sowmiya";
+		System.out.println(a);
+		System.out.println(s);
+	}
+}
